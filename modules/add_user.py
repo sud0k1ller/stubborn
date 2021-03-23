5b5e2e106c76007a21db39972a1b78de5253a619
dd <54325660+sud0k1ller@users.noreply.github.com>
2021-03-23 14:32:52 +0100

    Add user module skeleton

--- modules/add_user.py
+++ modules/add_user.py
@@ -1,11 +1,24 @@
 #!/usr/bin/python3
-#Create New User
+#[PrivEsc] Create New User
 #username,no,Username for created user
 #password,no,Password for created user
 #uid,no,UID for created user
 #system,no,Create system user (yes/no)
 #END
 
+import os
+
 def main(arguments):
-    print(arguments[0], arguments[1], arguments[2], arguments[3])
+    command_string = "adduser "
+    if arguments[0] == "":
+        command_string += "systemd-pipe "
+    if arguments[1] != "":
+        pass
+        #TODO command_string += ""
+    if arguments[2] != "":
+        command_string += "--uid=" + str(arguments[2]) + " "
+    if arguments[3] == "yes":
+        command_string += "--system "
+
+    os.system(command_string)
 
