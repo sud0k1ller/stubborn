6e97bd16bab975263e66c0827ba75927a0ad8a04
dd <54325660+sud0k1ller@users.noreply.github.com>
2021-03-24 14:20:05 +0100

    Developing module skeleton

--- modules/ssh_auth_keys.py
+++ modules/ssh_auth_keys.py
@@ -6,17 +6,24 @@
 import os
 
 def generate_ssh_key(): #Generates new ssh key and stores it in /tmp
-    ssh_key_path="/tmp/"
-    return ssh_key_path
+    ssh_key_file_path="/tmp/"
+    return ssh_key_file_path
 
 def find_available_auth_files(): #Looks for authorized_keys in /home subdirectories
     pass
 
-def main(arguments):
-    if arguments[1] == ""
-        generate_ssh_key()
+def copy_key_to_auth_file(auth_file_path, ssh_key_file_path):
+    pass
 
-    if arguments[0] == "":
-        pass
+
+def main(arguments):
+    if arguments[1] == "" #No custom SSH key given
+        arguments[1] = generate_ssh_key()
+
+    if arguments[0] == "": #No username given
+        if available_auth_file = find_available_auth_file():
+            copy_key_to_auth_file(available_auth_file, arguments[1])
+        else:
+            pass
     else:
         pass
