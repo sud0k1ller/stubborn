9ce55a9099025ce4275e23e44a5338d33c9563b6
dd <54325660+sud0k1ller@users.noreply.github.com>
2021-03-24 14:12:21 +0100

    Adding authorized ssh keys module skeleton

---
+++ modules/ssh_auth_keys.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+#[Access] SSH authorized_keys
+#USER,no,Username to whom keys will be added
+#KEY_PATH,no,Path to generated SSH key
+#END
+import os
+
+def generate_ssh_key(): #Generates new ssh key and stores it in /tmp
+    ssh_key_path="/tmp/"
+    return ssh_key_path
+
+def find_available_auth_files(): #Looks for authorized_keys in /home subdirectories
+    pass
+
+def main(arguments):
+    if arguments[1] == ""
+        generate_ssh_key()
+
+    if arguments[0] == "":
+        pass
+    else:
+        pass
